9e553c36e07793ce657af35b8f6f6d912932eccb
tannerfunk <88557372+tannerfunk@users.noreply.github.com>
2022-07-02 09:22:44 -0600

    up to before the modal window

--- js/scripts.js
+++ js/scripts.js
@@ -2,7 +2,8 @@ const gallery = document.querySelector('#gallery');
 
 
 const cardImgContainer = document.querySelector(".card-img-container");
-
+const body = document.getElementsByTagName("body");
+var card1 = '';
 
 const firstNames = [];
 const lastNames = [];
@@ -21,7 +22,7 @@ function left(str, chr) {
     return str.slice(0, chr - str.length);
   }
 
-fetch('https://randomuser.me/api/?results=12') //connects to the API
+fetch('https://randomuser.me/api/?nat=us&results=12') //connects to the API
     .then(response => response.json()) //returns a promise that resolves to JSON
     .then(data => {
         console.log(data) //to figure out what to set to the arrays
@@ -42,7 +43,7 @@ fetch('https://randomuser.me/api/?results=12') //connects to the API
         dateFixer = dateFixer[5] + dateFixer[6] + "/" +  dateFixer[8] + dateFixer[9] + "/" + dateFixer[0] + dateFixer[1]+ dateFixer[2] + dateFixer[3];
         birthdays[i] = dateFixer;
         gallery.insertAdjacentHTML('beforeend', `
-        <div class="card">
+        <div class="card num${i}">
             <div class="card-img-container">
                 <img class="card-img" src="${images[i]}" alt="profile pic">
             </div>
@@ -52,4 +53,37 @@ fetch('https://randomuser.me/api/?results=12') //connects to the API
                 <p class="card-text cap">${cities[i]}, ${states[i]}</p>
             </div>
         </div> `)
-        }})
+        
+        
+
+
+
+        }});
+
+    card1 = document.querySelector(`num1`);
+    card1.addEventListener('click', test);
+
+
+function displayModal(){
+
+    body.insertAdjacentHTML('beforeend', `
+    <div class="modal-container">
+                <div class="modal">
+                    <button type="button" id="modal-close-btn" class="modal-close-btn"><strong>X</strong></button>
+                    <div class="modal-info-container">
+                        <img class="modal-img" src="https://placehold.it/125x125" alt="profile picture">
+                        <h3 id="name" class="modal-name cap">name</h3>
+                        <p class="modal-text">email</p>
+                        <p class="modal-text cap">city</p>
+                        <hr>
+                        <p class="modal-text">(555) 555-5555</p>
+                        <p class="modal-text">123 Portland Ave., Portland, OR 97204</p>
+                        <p class="modal-text">Birthday: 10/21/2015</p>
+                    </div>
+                </div>
+    `)
+};
+
+function test(){
+    console.log("test");
+}
